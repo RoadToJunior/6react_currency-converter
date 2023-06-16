255c449c98ac68391bf3b4c1028289a8dab53738
NowWeAre-Free <kontodarm@wp.pl>
2023-06-16 19:26:26 +0200

    chodzi to jak w zegareczku

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
+const DollarCurrency = (props) => {
+  return (
+    <>
+      <p>$$$:{props.value * props.pln}</p>
+    </>
+  );
+};
+
+const EuroCurrency = (props) => {
+  return (
+    <>
+      <p>Euro:{props.value * props.pln}.</p>
+    </>
+  );
+};
+
 class ExchangeCounter extends React.Component {
+  state = {
+    number: 0,
+  };
+  handleNumberChange = (e) => {
+    this.setState({
+      number: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div className="">
         <label>
           Wpisz kwotę w złotówkach
-          <input type="number" />
+          <input type="number" onChange={this.handleNumberChange} />
         </label>
-        <DolarCurrency />
-        <EuroCurrency />
+        <DollarCurrency value={4.09} pln={this.state.number} />
+        <EuroCurrency value={4.47} pln={this.state.number} />
       </div>
     );
   }
